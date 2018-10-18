de105d07d6b9e72221264480da573509889e61f1
justcallmebeef <bethanymitch10@gmail.com>
2018-10-18 13:35:18 -0600

    added the body to the Message component

--- src/components/Message.js
+++ src/components/Message.js
@@ -4,26 +4,33 @@ import React from 'react';
 const Message = (props) => {
     return props.messages.map((message) => {
     return (
+        <>
         <div className={`row message ${message.read ? 'read' : 'unread' }`}>
-        <div className="col-xs-1">
-            <div className="row">
-            <div className="col-xs-2">
-                <input type="checkbox" />
+            <div className="col-xs-1">
+                <div className="row">
+                    <div className="col-xs-2">
+                        <input type="checkbox" />
+                    </div>
+                    <div className="col-xs-2" onClick={props.starClick}>
+                        <i className={`star fa ${message.starred ? 'fa-star' : 'fa-star-o'}`}></i>
+                    </div>
+                </div>
             </div>
-            <div className="col-xs-2" onClick={props.starClick}>
-                <i className={`star fa ${props.starMessage ? 'fa-star-o' : 'fa-star'}`}></i>
-            </div>
+            <div className="col-xs-11">
+                <span className="label label-warning">{message.labels[0]}</span>
+                <span className="label label-warning">{message.labels[1]}</span>
+                <span className="label label-warning">{message.labels[2]}</span>
+                <a href="#">
+                    {message.subject}
+                </a>
             </div>
         </div>
-        <div className="col-xs-11">
-            <span className="label label-warning">{message.labels[0]}</span>
-            <span className="label label-warning">{message.labels[1]}</span>
-            <span className="label label-warning">{message.labels[2]}</span>
-            <a href="#">
-            {message.subject}
-            </a>
+        <div className="row message-body hidden">
+            <div className="col-xs-11 col-xs-offset-1">
+                {message.body}
+            </div>
         </div>
-        </div>
+        </>
     )
     })
 }
