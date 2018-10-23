fb111db0f5988b843c24ad083160735923c70360
justcallmebeef <bethanymitch10@gmail.com>
2018-10-23 15:15:48 -0600

    updated the router so when a new message is built all of the messages get selected

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       messages: [],
       composeMessage: true,
       bodyMessage: true, 
-      selectButton: false,
+      checkButton: false,
       subject: "", 
       body: "",
     }
